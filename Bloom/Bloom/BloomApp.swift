27461701cc4959ded9ef42ed4a9a7aec1f0185e1
トム・クルーズ <tomukuruzu@tomukuruzunoMacBook-Air.local>
2024-02-20 21:14:13 +0900

    ファイル追加

---
+++ Bloom/Bloom/BloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  BloomApp.swift
+//  Bloom
+//
+//  Created by トム・クルーズ on 2024/02/20.
+//
+
+import SwiftUI
+
+@main
+struct BloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bloom/Bloom/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bloom
+//
+//  Created by トム・クルーズ on 2024/02/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
